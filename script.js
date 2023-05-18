36bfe5e18204b4606deaf4fb79a83c029db9e85a
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-05-18 15:17:53 -0500

    added div

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 var hoursArr = [00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 console.log(hoursArr);
-
+console.log($('body').children())
+var $newDiv = $("<div id='hour-'></div>");
+  newdiv2 = document.createElement("div");
+  existingDiv = document.getElementById("hour-")
 $(function () {
   var currHour = dayjs().hour();
   console.log(currHour)
   $('.time-block').each(function() {
-    var id = $(this).attr('id').split('-')[1];
+    for (var i = 0; i < hoursArr.length; i++) {
+      $('#hour-').attr('id', hoursArr[i]);
+      hoursArr[i]++
+    };
+    var id = $(this).attr('id');
     var rowHour = parseInt(id);
     console.log(rowHour);
-    
+    $('#time').text(rowHour + ":00");
 
     if (currHour < rowHour) {
       $(this).addClass('future');
@@ -21,6 +29,8 @@ $(function () {
       $(this).addClass('present');
     };
   });
+  $('body').children('div').eq(4).append($newDiv, [newdiv2, existingDiv]);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
